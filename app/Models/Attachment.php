7e39cc8750e257fea834688061afff0924f867c5
Wilsen Hernández <wilsenh95@gmail.com>
2021-09-06 15:04:10 -0400

    Defined new Attachment model and migration

---
+++ app/Models/Attachment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attachment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'attachable_id',
+        'attachable_type',
+        'url',
+    ];
+
+    public function attachable()
+    {
+        return $this->morphTo();
+    }
+}
