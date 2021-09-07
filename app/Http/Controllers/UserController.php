9239cab568c28d30bee7f37d1c8c2a1162f66413
jbarretodev <jbarretodev6105@gmail.com>
2021-09-07 16:05:42 -0400

    refactor code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,8 +9,7 @@ class UserController extends Controller
 {
     public function index(){
         $users = User::with(["posts" => function($query){
-            $query->withCount('post_attachments');
-            $query->withCount('comments');
+            $query->withCount('post_attachments')->withCount('comments');
         },"posts.comments" => function($query){
             $query->withCount('comment_attachments');
         }])->get();
