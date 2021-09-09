bfb00c5e756e3cc96e3de8f5914094bd325eb836
Haxiel Malave <hmalave@dannyyesoft.mx>
2021-09-08 21:31:39 -0400

    Adaptacion del controlador PostController al repositorio de usuarios

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use DB;
+use App\Repositories\Application\UserRepository;
 
 class PostController extends Controller
 {
+    protected $repUser;
+
+    public function __construct(UserRepository $repUser)
+    {
+        $this->repUser = $repUser;
+    }
+
     public function index()
     {
+        $users = $this->repUser->getUsersWithPosts();
+
         return view('index', [
-            'users' => \App\Models\User::all()
+            'users' => $users
         ]);
     }
 }
